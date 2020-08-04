c916a16450aff6954d11d28ea5674e2c9c837b28
anthony <necrodeathastm@gmail.com>
2020-08-04 15:44:06 -0500

    04082020

--- System/src/routes/Manto_Tablas.js
+++ System/src/routes/Manto_Tablas.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-const pool_db = require('../database');
 const { route } = require('.');
 const pool = require('../database');
 
 
-router.get('/add_ciclo', ( req,res ) => {
-    
+router.get('/add_ciclo', ( req,res ) => {    
     res.render('tablas/add_ciclo');
+});
+
+router.post('/add_ciclo', async (req,res) => {
+    /*console.log(req.body); */ //-> Recupero datos del body
+
+    // Atributos Name en el body add.hbs
+    const {  ciclo, anio , fec_ini, fec_fin} = req.body;
+  
+    await pool.query('CALL Tsp_insert_link (?, ?, ?, ?)', [ciclo, anio , fec_ini, fec_fin,req.user.id],(err,rows,fields) => {
+        if(!err)
+        {
+            req.flash('Exito','Agregado correctamente');//MSG
+            res.redirect('/links');
+            
+        }else{
+            console.log(err);
+        }
+    } )
+        
 });
 
 
